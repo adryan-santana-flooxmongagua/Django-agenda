22075ad3020e36c2402d5e7c13eec47ce4d6c4a8
Adryan Alex. <adryan.silva.flooxmongagua@gmail.com>
2025-07-03 11:42:45 -0300

    feat: create view, URL and template for single contact

--- contact/urls.py
+++ contact/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from contact.views import contact_views
+from contact import views
 
 app_name = 'contact'
 
 urlpatterns = [
-    path('', contact_views.index, name='index'),
+    path('<int:contact_id>/', views.contact, name='contact'),  # type:ignore
+    path('', views.index, name='index'),  # type:ignore
 ]
